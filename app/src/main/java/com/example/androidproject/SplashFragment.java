202f100813b79a9485f088011aa69041b07d0f36
jdunlap22 <97454806+jdunlap22@users.noreply.github.com>
2022-12-01 20:31:52 -0400

    first

---
+++ app/src/main/java/com/example/androidproject/SplashFragment.java
@@ -0,0 +1,21 @@
+package com.example.androidproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SplashFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+}

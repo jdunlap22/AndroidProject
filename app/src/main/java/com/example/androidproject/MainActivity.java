e29ac2b607dcecaf0ee644dc0f3e254468ed830d
jdunlap22 <97454806+jdunlap22@users.noreply.github.com>
2022-12-01 21:46:10 -0400

    NSCC Contact App

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.androidproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavView;
+
+    SplashFragment splashFragment = new SplashFragment();
+    AddContactFragment addContactFragment = new AddContactFragment();
+    RecyclerFragment recyclerFragment = new RecyclerFragment();
+    CreditsFragment creditsFragment = new CreditsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavView = findViewById(R.id.nav_view);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,splashFragment).commit();
+
+        bottomNavView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.nav_splash) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,splashFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.nav_contacts_form) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,addContactFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.nav_recycler_view) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,recyclerFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.nav_credits) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,creditsFragment).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
 }

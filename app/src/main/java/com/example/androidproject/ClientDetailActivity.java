6c0d176f278745286792b340d68bf6f270c1a8b5
jdunlap22 <97454806+jdunlap22@users.noreply.github.com>
2022-12-04 22:53:10 -0400

    NSCC Contact App

---
+++ app/src/main/java/com/example/androidproject/ClientDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClientDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_detail);
+    }
+}
--- app/src/main/java/com/example/androidproject/RecyclerFragment.java
+++ app/src/main/java/com/example/androidproject/RecyclerFragment.java
@@ -1,19 +1,40 @@
 package com.example.androidproject;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.androidproject.databinding.FragmentRecyclerBinding;
+import com.google.firebase.database.Query;
+
 public class RecyclerFragment extends Fragment {
 
+    RecyclerView recyclerView;
+    private FragmentRecyclerBinding binding;
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recycler, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        binding = FragmentRecyclerBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        recyclerView = root.findViewById(R.id.recycler_view);
+        setupRecyclerView();
+        return root;
+    }
+
+    void setupRecyclerView() {
+        Query query = Utility.getCollectionReferenceForNotes().orderBy("Last Name", Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Contacts>
     }
 }
